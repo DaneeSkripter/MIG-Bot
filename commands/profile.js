0180c98374f4919e66dbddd174b4fd21e0a01889
daneeskripter <imdanee@seznam.cz>
2023-06-25 16:43:00 +0200

    some bug fix

--- commands/profile.js
+++ commands/profile.js
@@ -12,7 +12,6 @@ new Command({
             description: "User to show his profile",
             type: ArgumentType.USER,
             required: true
-
         })
     ],
 	run: async (ctx) => {
@@ -23,7 +22,8 @@ new Command({
         let profiles = ""
         if (!profile) {
             profiles = "<:error:1122470917163982858> User's profile is empty."
-        } else if (profile.minecraftusername) {
+        } else {
+        if (profile.minecraftusername) {
             profiles = `${profiles}\n<:minecraft:1122462846811390053> Minecraft: ${profile.minecraftusername}`
         } if (profile.wotusername) {
             profiles = `${profiles}\n<:wot:1122464725582745720> World Of Tanks: ${profile.wotusername}`
@@ -33,9 +33,13 @@ new Command({
             profiles = `${profiles}\n<:fortnite:1122466893014519889> Fortnite: ${profile.fortniteusername}`
         } if (profile.wowusername) {
             profiles = `${profiles}\n<:wow:1122467632000536697> World Of Warcraft: ${profile.wowusername}`
+        } if (profiles == "") {
+            profiles = "<:error:1122470917163982858> User's profile is empty."
         }
+    }
         embed.setDescription(profiles)
         embed.setColor("Random")
+        embed.setFooter({ text: "⚠️: The usernames listed above may not actually belong to this user." })
         ctx.reply({embeds: [embed]})
 	}
 });
